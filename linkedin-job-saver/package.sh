d7eb65ddb57a1f3044ed20ea589a0733aec85482
Mark Hasse <mhasse@gmail.com>
2025-03-26 10:32:13 -0500

    v1.1 Manifest corrections

---
+++ linkedin-job-saver/package.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zip -r LinkedInJobSaver.zip * -x "*.git*" "*.DS_Store" "README.md" "*zip" "*~" "*sh"
--- package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ce LinkedIn-Job_Saver
-zip -r linkedin-job-downloader.zip * -x "*.git*"
-mv linkedin-job-downloader.zip ..
-cd ..
